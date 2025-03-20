50dc823de87163ebccfc65025ab9b92fec753c1a
Mishanya <mihailziganov1811@gmail.com>
2025-03-20 20:50:20 +0400

    add метод-проверка

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en')
 
 
-@pytest.fixture()
+@pytest.fixture(scope='function')
 def browser(request):
     language = request.config.getoption('language')
     options = Options()
